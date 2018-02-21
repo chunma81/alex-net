cec961b70144ed03caff36b30e023674de3a4ed0
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-02-21 15:58:54 +0900

    ping check for TCP port

--- ping-tcp.py
+++ ping-tcp.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python2.7
-import os
+import subprocess
+
 
 def HTTP_80():
     HTTP = raw_input("TCP80 Host IP: ")
-    bashcommand = "tcping -p 80 -i 0.5 -c 5 -h "+HTTP
-    os.system (bashcommand)
+    bashcmd1 = "tcping -p 80 -i 0.5 -c 5 -h "+HTTP
+    subprocess.call(bashcmd1, shell=True)
 
 def HTTPS_443():
     SSL = raw_input("TCP443 Host IP: ")
-    bashcommand = "tcping -p 443 -i 0.5 -c 5 -h "+SSL
-    os.system (bashcommand)
+    bashcmd2 = "tcping -p 443 -i 0.5 -c 5 -h "+SSL
+    subprocess.call(bashcmd2, shell=True)
 
 def DNS_53():
     DNS = raw_input("TCP53 Host IP: ")
-    bashcommand = "tcping -p 53 -c 5 -i 0.5 -h "+DNS
-    os.system (bashcommand)
+    bashcmd3 = "tcping -p 53 -c 5 -i 0.5 -h "+DNS
+    subprocess.call(bashcmd3, shell=True)
+
 
 print("############# Select Mode #########################")
 print("### [1] TCP 80  [2] TCP 443 [3] DNS 53 [4] EXIT ###")
