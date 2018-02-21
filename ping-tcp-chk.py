e0de87ddc39435ee8beac4190cc1628db2503203
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-02-21 14:38:16 +0900

    ping check for TCP port

--- ping-tcp-chk.py
+++ ping-tcp-chk.py
@@ -3,22 +3,21 @@ import os
 
 def HTTP_80():
     HTTP = raw_input("TCP80 Host IP: ")
-    bashcommand = "tcping -p 80 -c 3 -h "+HTTP
+    bashcommand = "tcping -p 80 -i 0.5 -c 5 -h "+HTTP
     os.system (bashcommand)
 
 def HTTPS_443():
     SSL = raw_input("TCP443 Host IP: ")
-    bashcommand = "tcping -p 443 -c 3 -h "+SSL
+    bashcommand = "tcping -p 443 -i 0.5 -c 5 -h "+SSL
     os.system (bashcommand)
 
-def OTHERS():
-    PORT = raw_input("What TCP Port: ")
-    HOST = raw_input("Host IP: ")
-    bashcommand = "tcping -c 3 -p "+PORT -h +HOST
+def DNS_53():
+    DNS = raw_input("TCP53 Host IP: ")
+    bashcommand = "tcping -p 53 -c 5 -i 0.5 -h "+DNS
     os.system (bashcommand)
 
 print("############# Select Mode #########################")
-print("### [1] TCP 80  [2] TCP 443 [3] OTHER  [4] EXIT ###")
+print("### [1] TCP 80  [2] TCP 443 [3] DNS 53 [4] EXIT ###")
 print("###################################################")
 Menu = raw_input("Enter Number of Mode: ")
 if Menu == "1" :
@@ -26,7 +25,7 @@ if Menu == "1" :
 elif Menu == "2" :
     HTTPS_443()
 elif Menu == "3" :
-    OTHERS()
+    DNS_53()
 elif Menu == "4" :
     pass
 else:
