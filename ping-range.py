d06ae67b458197b56ff218d4e9f3a914b3d86cc6
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-02-21 16:25:27 +0900

    range ping for some IP Block

--- ping-range.py
+++ ping-range.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python2.6
-import subprocess
+#!/usr/bin/python2.7
 import os
+import subprocess
+
+
 with open(os.devnull, "wb") as limbo:
-        for n in xrange(1, 254):
-                ip="123.103.50.{0}".format(n)
-                result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", ip],
+        for n in xrange(1, 9):
+                ip="8.8.8.{0}".format(n)
+                result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "3", ip],
                         stdout=limbo, stderr=limbo).wait()
                 if result:
                         print ip, "is NOT Reachable"
