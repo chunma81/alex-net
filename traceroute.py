93691be5d9a102cd45850fabd74a92bc00d3f1e8
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-02-21 15:53:25 +0900

    check for routing-path Traceroute/MTR

--- traceroute.py
+++ traceroute.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python2.7
-import os
+import subprocess
+
 
 def TRACE():
     IP1 = raw_input("DST IP Address:")
-    bashcommand = "traceroute -n -w 1 "+IP1 
-    os.system (bashcommand)
+    bashcmd1 = "traceroute -n -w 1 "+IP1 
+    subprocess.call(bashcmd1, shell=True)
 
 def MTR():
     IP2 = raw_input("DST IP Address:")
-    bashcommand = "mtr -n -r -c 1 "+IP2
-    os.system (bashcommand)
+    bashcmd2 = "mtr -n -r -c 1 "+IP2
+    subprocess.call(bashcmd2, shell=True)
+
 
 print("############# Select Mode #################")
 print("####[1] TraceRoute [2] MTR  [3] EXIT ######")
