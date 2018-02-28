9b78cc2fab6ce920a6d4a527271f80987440e081
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-02-28 14:41:14 +0900

    ping check for checklists file

--- ping.py
+++ ping.py
@@ -3,11 +3,11 @@ import os
 import subprocess
 
 
-with open('checklists.txt', 'r') as f:
+with open('pinglists.txt', 'r') as f:
     for ip in f:
         result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "3", ip],stdout=f, stderr=f).wait()
         if result:
-            print (ip, "is NOT Reachable")
+            print (ip.replace("\n",""),"is NOT Reachable")
         else:
-            print (ip, "is Reachable")
+            print (ip.replace("\n",""),"is Reachable")
 
