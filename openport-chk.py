deca2dc65a496df213b6feccb8eb21136c2ca9b1
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-02-28 18:38:22 +0900

    port check for TCP/UDP (Telnet,FTP,NTP,HTTP,SNMP)

--- openport-chk.py
+++ openport-chk.py
@@ -15,17 +15,27 @@ def TCP_OT():
     print result2.communicate()[0]
 
 
-print("#######################Select Mode ##########################")
-print("### [1] UDP Port Check    [2] TCP Port Check     [3] EXIT ###")
-print("#############################################################")
+def UDP_LIST():
+    UDP = raw_input("UDP Port Number: ")    
+    with open('portchklists.txt', 'r') as f:
+        for SWL in f:
+            result3 = subprocess.Popen(["nc", "-uzv", SWL.replace("\n",""), UDP, "-w", "3"],stdout=subprocess.PIPE)
+            print result3.communicate()[0]
+
+
+print("########################################Select Mode ##########################################################")
+print("### [1] UDP Onetime Check     [2] TCP Onetime Check     [3] UDP Check for lists swtiches         [4] EXIT ###")
+print("##############################################################################################################")
 Menu = raw_input("Enter Number of Mode: ")
 if Menu == "1" :
     UDP_OT()
 elif Menu == "2" :
     TCP_OT()
 elif Menu == "3" :
+    UDP_LIST()
+elif Menu == "4" :
     pass
 else:
-    print("Plaese Select")
+    print("Exit!Plaese try to Select")
 
 
