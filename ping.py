18f8a1fca8175a8917e19f47500b7453d4f70380
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-02-28 15:07:03 +0900

    automation ping check for checklists file

--- ping.py
+++ ping.py
@@ -7,7 +7,7 @@ with open('pinglists.txt', 'r') as f:
     for ip in f:
         result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "3", ip],stdout=f, stderr=f).wait()
         if result:
-            print (ip.replace("\n",""),"is NOT Reachable")
+            print ip.replace("\n",""),"is NOT Reachable"
         else:
-            print (ip.replace("\n",""),"is Reachable")
+            print ip.replace("\n",""),"is Reachable"
 
