a4b52b95a4db5e5c7272c45867a8eca48a488a0c
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-03-01 16:45:47 +0900

    port check for TCP/UDP (Telnet,FTP,NTP,HTTP,SNMP)

--- openport-chk.py
+++ openport-chk.py
@@ -14,7 +14,6 @@ def TCP_OT():
     result2 = subprocess.Popen(["nc", "-zv", SW2, TCP, "-w", "3"],stdout=subprocess.PIPE)
     print result2.communicate()[0]
 
-
 def UDP_LIST():
     UDP = raw_input("UDP Port Number: ")    
     with open('portchklists.txt', 'r') as f:
@@ -22,9 +21,16 @@ def UDP_LIST():
             result3 = subprocess.Popen(["nc", "-uzv", SWL.replace("\n",""), UDP, "-w", "3"],stdout=subprocess.PIPE)
             print result3.communicate()[0]
 
+def TCP_LIST():
+    TCP = raw_input("TCP Port Number: ")
+    with open('portchklists.txt', 'r') as f:
+        for SWL in f:
+            result4 = subprocess.Popen(["nc", "-zv", SWL.replace("\n",""), TCP, "-w", "3"],stdout=subprocess.PIPE)
+            print result4.communicate()[0]
 
-print("########################################Select Mode ##########################################################")
-print("### [1] UDP Onetime Check     [2] TCP Onetime Check     [3] UDP Check for lists swtiches         [4] EXIT ###")
+
+print("########################################### Select Mode ######################################################")
+print("###  [1] UDP Check    [2] TCP Check    [3] UDP Check for SWlists    [4] TCP Check for SWlists    [5] EXIT  ###")
 print("##############################################################################################################")
 Menu = raw_input("Enter Number of Mode: ")
 if Menu == "1" :
@@ -34,6 +40,8 @@ elif Menu == "2" :
 elif Menu == "3" :
     UDP_LIST()
 elif Menu == "4" :
+    TCP_LIST()
+elif Menu == "5" :
     pass
 else:
     print("Exit!Plaese try to Select")
