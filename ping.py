7573ed6e4cb8c524b7c669ae33346601df768a77
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-03-03 15:53:26 +0900

    automation ping check for checklists file (ICMP/TCP)

--- ping.py
+++ ping.py
@@ -15,15 +15,18 @@ def PING_TCP():
     TCP = raw_input("TCP Port Number: ") 
     with open('pinglists.txt', 'r') as f:
         for IP in f:
-            result = subprocess.Popen(["tcping", "-p", TCP, "-c", "3", "-i", "0.5", "-h", IP],stdout=f, stderr=f).wait()            
-            if result:
-                print IP.replace("\n",""),"is NOT Reachable"
-            else:
+            result = subprocess.Popen(["tcping", "-t", "1", IP, TCP],stdout=subprocess.PIPE, stderr=subprocess.PIPE,bufsize=1)
+            result_string = ''
+            for i in iter(result.stdout):
+                result_string += bytes.decode(i).split('\n')[0]
+            if 'port '+TCP+' open' in result_string:
                 print IP.replace("\n",""),"is Reachable"
+            else:
+                print IP.replace("\n",""),"is NOT Reachable"
 
 
 print("################### Select Mode ####################")
-print("### [1] PING        [2] TCP PING        [3] EXIT ###")
+print("###   [1] PING      [2] TCP PING     [3] EXIT    ###")
 print("####################################################")
 Menu = raw_input("Enter Number of Mode: ")
 if Menu == "1" :
