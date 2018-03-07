03b515710895116e334419385f9d9a663de3f1bc
Hunkeun.Ha <hunkeun.ha@gmail.com>
2018-03-07 17:35:09 +0900

    check for whois IP/ASN, RADB.net and ALTDB.net

--- whois.py
+++ whois.py
@@ -17,9 +17,14 @@ def RADB_BL():
     bashcmd = "whois -h whois.radb.net "+IP
     subprocess.call(bashcmd, shell=True)
 
-print("######################## Select Mode #########################")
-print("### [1] Whois ASN [2] Whois IP [3] RADB.NET BLOCK [4] EXIT ###")
-print("##############################################################")
+def ALTDB_BL():
+    IP = raw_input("IPv4 Block Address: ")
+    bashcmd = "whois -h whois.altdb.net "+IP
+    subprocess.call(bashcmd, shell=True)
+
+print("############################# Select Mode ################################")
+print("### [1] Whois ASN  [2] Whois IP  [3] RADB.net  [4] ALTDB.net  [5] EXIT ###")
+print("##########################################################################")
 Menu = raw_input("Enter Number of Mode: ")
 if Menu == "1" :
     WHOIS_AS()
@@ -28,6 +33,8 @@ elif Menu == "2" :
 elif Menu == "3" :
     RADB_BL()
 elif Menu == "4" :
+    ALTDB_BL()
+elif Menu == "5" :
     pass
 else:
     print("Exit!Plaese try to Select")
